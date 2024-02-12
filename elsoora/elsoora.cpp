9af3afa11da84136af4b329cdc84b268a380a099
InClass <hallgato@LABOR110-08>
2024-02-12 17:19:16 +0100

    Reference Learning

--- elsoora/elsoora.cpp
+++ elsoora/elsoora.cpp
@@ -1,5 +1,5 @@
 // elsoora.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//I. Forditoas
+//I. Forditás
 // 1 cpp állományt fordítunk le
 //      1. Precompile
 //          - #kezdeû sorokat értelmezi és végrehajtja
@@ -15,11 +15,61 @@
 
 #include <iostream>
 
-int main() //fugveny kell return de a mainnel nem
-{
-    std::cout << "Hello World!\n";
+//cppreference.com
 
-    return 0; //alapbol 0val tér vissza intként
-    
+
+
+int main() //fugveny kell return de a mainnel nem
+{/*
+	std::string firstName;
+	std::cout << "What is your first name? ";
+	std::cin >> firstName;
+	std::cout << "Hello " << firstName << "!";
+
+	int a;
+	std::cin >> a;*/
+
+	///*int a = 5u;
+	//unsigned b;
+	//short c;
+	//unsigned short int d;
+	//long int e;
+	//unsigned long int f;
+	//long long g;
+	//char h;*/
+
+	// Egész típusok C++11 óta
+	//int8_t a;
+	//uint8_t b;
+	//size_t c; //meret indexelo altalaban
+
+	//lebegõpontos típusok
+	float f = 3.14;
+	double d = 3.14;
+
+	//logikai típus
+	bool baba = true; //false
+
+	baba = 56;
+
+	
+	/*int n = 10;
+	while (n--)
+	{
+		std::cout << n << std::endl;
+	}*/
+	int a = 10;
+
+	while (std::cin >> a, a)
+	{
+		std::cout << a << std::endl;
+	}
+
+	//// referencia típus
+	//int a = 5;
+	//int& b = a;
+
+	const int b = 5;
+	
 }
 

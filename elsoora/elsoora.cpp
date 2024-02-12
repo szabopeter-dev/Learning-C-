a2d8d8fd81d3eb85339c0fbe34b4ef326d2e1563
InClass <hallgato@LABOR110-08>
2024-02-12 16:15:05 +0100

    Theory

--- elsoora/elsoora.cpp
+++ elsoora/elsoora.cpp
@@ -3,8 +3,14 @@
 // 1 cpp állományt fordítunk le
 //      1. Precompile
 //          - #kezdeû sorokat értelmezi és végrehajtja
+//          -szintaktikai ellenõrzés (fentrõl - lefelé)
 //      2. Compile
+//          -lefordítja a kíbõvített cpp állományt
+//          - => object fájlt állít elõ
 //II. Linkelés
+//              -Minden cpp állomány önállóan lefordítható
+//              - *.cpp => *.o
+//              -Object állománokat össze kell linkelni => .exe
 
 
 #include <iostream>

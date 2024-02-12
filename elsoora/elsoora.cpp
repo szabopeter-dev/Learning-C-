3c7f918754a73f48db5cc5a82f48b0b8804e37e9
hallgato <hallgato@LABOR110-08>
2024-02-12 16:06:31 +0100

    Add project files.

---
+++ elsoora/elsoora.cpp
@@ -0,0 +1,19 @@
+// elsoora.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//I. Forditoas
+// 1 cpp állományt fordítunk le
+//      1. Precompile
+//          - #kezdeû sorokat értelmezi és végrehajtja
+//      2. Compile
+//II. Linkelés
+
+
+#include <iostream>
+
+int main() //fugveny kell return de a mainnel nem
+{
+    std::cout << "Hello World!\n";
+
+    return 0; //alapbol 0val tér vissza intként
+    
+}
+
